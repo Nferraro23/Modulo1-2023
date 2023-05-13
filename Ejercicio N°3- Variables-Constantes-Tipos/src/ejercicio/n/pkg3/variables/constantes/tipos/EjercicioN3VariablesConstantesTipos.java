50da994bdabff00475299d9929ad432a7a2f4dae
Nferraro23 <128649208+Nferraro23@users.noreply.github.com>
2023-05-13 12:08:41 -0300

    Terminado

--- Ejercicio N°3- Variables-Constantes-Tipos/src/ejercicio/n/pkg3/variables/constantes/tipos/EjercicioN3VariablesConstantesTipos.java
+++ Ejercicio N°3- Variables-Constantes-Tipos/src/ejercicio/n/pkg3/variables/constantes/tipos/EjercicioN3VariablesConstantesTipos.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio.n.pkg3.variables.constantes.tipos;
 
-/**
- *
- * @author PabloLopez
- */
 public class EjercicioN3VariablesConstantesTipos {
 
     /**
